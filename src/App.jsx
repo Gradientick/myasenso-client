9a579f30466c9c10a616ca66ac440084b178a857
gradientick <camus.luis95@gmail.com>
2023-06-08 10:56:22 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import "./style.css";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="page-title">MyAsenso</h1>
+      <div className="header">
+        <div className="logo">My LOGO</div>
+        <div className="header-container">
+          <div className="left">
+            <h2>Hi, Welcome to MyAsenso!</h2>
+          </div>
+          <div className="right"></div>
+        </div>
+      </div>
+      <div className="main">
+        <div className="card">
+          <div className="frame"></div>
+          <div className="card-text"></div>
+          <div className="card-text"></div>
+          <div className="card-text"></div>
+          <div className="card-text"></div>
+          <div className="card-text"></div>
+          <div className="card-text"></div>
+        </div>
+        <div className="card">
+          <div className="frame"></div>
+          <div className="card-text"></div>
+          <div className="card-text"></div>
+          <div className="card-text"></div>
+        </div>
+        <div className="card">
+          <div className="frame"></div>
+          <div className="card-text"></div>
+          <div className="card-text"></div>
+          <div className="card-text"></div>
+          <div className="card-text"></div>
+          <div className="card-text"></div>
+          <div className="card-text"></div>
+        </div>
+      </div>
+      <div className="footer">FOOTER</div>
+    </>
+  );
+}
