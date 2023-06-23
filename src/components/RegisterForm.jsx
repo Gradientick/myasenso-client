2966c900336a7fb52a5a0e045fa56c5aa50d58ca
gradientick <camus.luis95@gmail.com>
2023-06-23 22:50:49 +0800

    added business name on registerForm Parameter

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,24 +1,26 @@
 import { useState } from "react";
 import registerService from "../services/registerService";
-import { useNavigate } from "react-router-dom";
+
 import "./LoginForm.css";
 function RegisterForm({ onFormSwitch }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
-  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+
   const handleRegister = (e) => {
     e.preventDefault();
     registerService
-      .register({ email, password, name, number })
+      .register({ email, password, name, number, title })
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
-    onFormSwitch("login");
-    setEmail("");
-    setPassword("");
-    setName("");
-    setNumber("");
+    // onFormSwitch("login");
+    // setEmail("");
+    // setPassword("");
+    // setName("");
+    // setNumber("");
+    // setTitle("");
   };
 
   return (
@@ -43,6 +45,14 @@ function RegisterForm({ onFormSwitch }) {
         placeholder="phone number"
         value={number}
         onChange={(e) => setNumber(e.target.value)}
+      />
+
+      <input
+        type="text"
+        className="email-input"
+        placeholder="Business Name"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
 
       <input
